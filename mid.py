3e54065bddd24f88ef0a638576644cce04f40688
Yuvaan8 <yuvaanbhandari007@gmail.com>
2026-06-16 18:48:54 +0530

    initial commit

--- mid.py
+++ mid.py
@@ -1,13 +1,13 @@
-num = int(input('Enter the number:'))
+num = int(input('Enter a 4 digit or more than 4 digit number:'))
 t = num
 numLen = 0
 while t>0:
      numLen = numLen+1
      t = int(t/10)
 if numLen >=4:
-     numLen = int(numLen/2)
-     chk = 0
-     while num>0:
+    numLen = int(numLen/2)
+    chk = 0
+    while num>0:
         rem = num%10
         if chk == numLen:
          midone = rem
@@ -15,10 +15,10 @@ if numLen >=4:
             midtwo = rem
         num = int(num/10)
         chk = chk + 1
-        prod = midone*midtwo
-        print('\nProduct of Mid digits('+str(midone)+ '*'+str(midtwo)+') =',prod)
-     else:
-         print('\nIts not a 4 or more than 4 digit number')
+    prod = midone*midtwo
+    print('\nProduct of Mid digits('+str(midone)+ '*'+str(midtwo)+') =',prod)
+else:
+    print('\nIts not a 4 or more than 4 digit number')
 
     
         
